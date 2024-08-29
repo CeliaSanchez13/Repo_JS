f4d53dd4e52ab598da1f2e808563c4ad0042def2
Celia Sánchez <celiasanchez4ac@gmail.com>
2024-08-29 18:33:34 +0200

    first commit

---
+++ operadores_asignacion.js
@@ -0,0 +1,31 @@
+// Operadores de Asignacion
+let miNumero = 10;
+console.log(miNumero); //10
+
+miNumero = 20;
+console.log(miNumero); //20
+
+// Operador +=
+miNumero += 5;
+console.log(miNumero); //25
+
+// Operador -=
+miNumero -= 3;
+console.log(miNumero); //23
+
+// Operador *=
+miNumero *= 2;
+console.log(miNumero); //44
+
+// Operador /=
+miNumero /= 4;
+console.log(miNumero); //11
+
+// Operador %=
+miNumero %= 2;
+console.log(miNumero); // 1 
+
+// Exponente **=
+miNumero = 8;
+miNumero **= 2;// 8 * 8 = 64
+console.log(miNumero);
