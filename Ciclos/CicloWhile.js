c58e1696345140c05b74952635a0f1a2489372aa
Celia Sánchez <celiasanchez4ac@gmail.com>
2024-10-06 19:07:34 +0200

    ciclos while

--- Ciclos/CicloWhile.js
+++
@@ -1,7 +0,0 @@
-// Ciclo while
-// Imprimir los valores del 0 al 5
-let contador = 0, repeticiones = 5;
-while(contador <= repeticiones){
-    console.log(contador++);
-    //contador++;
-}
---
+++ Ciclos/CicloWhileDo.js
@@ -0,0 +1,14 @@
+// Ciclo WHILE
+// Imprimir los valores del 0 al 5
+let contador = 0, repeticiones = 5;
+while(contador <= repeticiones){
+    console.log(contador++);
+    //contador++;
+}
+
+// Ciclo DO WHILE
+let contador = 0, repeticiones = 5;
+do{
+    console.log(contador++);
+    //contador++;
+}while(contador <= repeticiones)
